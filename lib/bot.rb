22211a599689fa8400d1fab47559ca7231640c23
Luiz Picolo <luizpicolo@gmail.com>
2020-03-25 11:30:03 -0400

    Created variable to contain the path to the database file

--- lib/bot.rb
+++ lib/bot.rb
@@ -6,19 +6,20 @@ require 'date'
 require 'time'
 
 class Bot
-	def initialize
-		@client = $client
+	def initialize(path_file)
+    @client = $client
+    @path_file = path_file
   end
   
   def last_date
-    File.open("db/out.txt", "a+") do |f| 
+    File.open(@path_file, "a+") do |f| 
       return Time.parse(f.readlines.map(&:chomp).last)
     end
   end
 
   def lasted_publications(amount)
     publictions = []
-    File.open("db/out.txt", "a+") do |f| 
+    File.open(@path_file, "a+") do |f| 
       f.readlines.reverse.each_with_index do |value, index|
         publictions << value.delete!("\n")
         break if index == amount
@@ -29,13 +30,13 @@ class Bot
   end
 
   def add_last_date(pub_date)
-    File.open("db/out.txt", "a+") do |f| 
+    File.open(@path_file, "a+") do |f| 
       f.puts pub_date.to_s
     end
   end
 
   def find_pub_date(pub_date)
-    File.open("db/out.txt", "a+") do |f| 
+    File.open(@path_file, "a+") do |f| 
       f.each do |line| 
         line.chomp!
         return true if line.include? pub_date.to_s
