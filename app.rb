49d065428f880d0c3847528c122b31154378f9ca
Luiz F. Picolo <luizpicolo@gmail.com>
2022-06-18 13:27:18 -0400

    Changed name file init.rb to app.rb

--- app.rb
+++ app.rb
@@ -2,6 +2,6 @@ require 'sinatra'
 require_relative 'lib/bot'
 
 get '/' do
-  @publications = Bot.new.lasted_publications(6)
+  @publications = Bot.new('db/out.txt').lasted_publications(6)
   erb :index 
 end
